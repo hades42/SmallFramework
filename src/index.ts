6483ddb6e4ad5477e83adec907daa2633b6dd602
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2021-11-18 12:24:33 +1100

    feat: testing api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello World");
+import { User } from "./models/User";
+import axios from "axios";
+
+const entryPoint = "http://localhost:3000";
+
+axios
+    .get(`${entryPoint}/users/1`, {})
+    .then((res) => {
+        console.log(res.data);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
