8359039d656ee5734e09d490595c70eeedd0c4b2
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2021-11-22 11:17:38 +1100

    feat: add more functions to User to use the functions of other classes

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,6 +2,7 @@ import axios, { AxiosResponse } from "axios";
 import { entryPoint } from "../index";
 import { Eventing } from "./Eventing";
 import { Sync } from "./Sync";
+import { Attribute } from "./Attribute";
 
 export interface UserProps {
     // "?" means that this interface can have these propreties,
@@ -18,12 +19,53 @@ export class User {
     // of event, we should use a composisiton pattern, by accepting Eventing
     // class as a paramater for constructor
     public events: Eventing = new Eventing();
-
     public sync: Sync<UserProps> = new Sync<UserProps>(entryPoint);
+    public attributes: Attribute<UserProps>;
 
-    private data: UserProps;
+    constructor(attrs: UserProps) {
+        this.attributes = new Attribute<UserProps>(attrs);
+    }
 
-    constructor(data: UserProps) {
-        this.data = data;
+    // Return the reference of the function
+    get on() {
+        return this.events.on;
+    }
+
+    // Return the reference of the function
+    get trigger() {
+        return this.events.trigger;
+    }
+
+    // Return the reference of the function
+    get get() {
+        return this.attributes.get.bind(this.attributes);
+    }
+
+    set(update: UserProps): void {
+        this.attributes.set(update);
+        this.events.trigger("change");
+    }
+
+    fetch(): void {
+        const id = this.get("id");
+
+        if (typeof id !== "number") {
+            throw new Error("Cannot fetch without an id");
+        }
+
+        this.sync.fetch(id).then((res: AxiosResponse): void => {
+            this.set(res.data);
+        });
+    }
+
+    save(): void {
+        this.sync
+            .save(this.attributes.getAll())
+            .then((res: AxiosResponse): void => {
+                this.trigger("save");
+            })
+            .catch(() => {
+                this.trigger("error");
+            });
     }
 }
